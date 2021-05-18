de69df818d96fc23270e9d7780625a917a56cad7
maratbyte <maratbyte@ya.ru>
2021-05-18 08:23:56 +0300

    Made changes accoring to Ilya's comments: used  instead of ; used  instead of

--- scripts/script.js
+++ scripts/script.js
@@ -4,37 +4,38 @@ const append = document.getElementById("append-button");
 const input = document.getElementById("append-input");
 
 // Adding tasks
-append.onclick = function() {
+function addTask() {
     // Storing the current value of the input field
-    let value = input.value;
+    const value = input.value;
     input.value = "";
 
     
     // => construct an item element <=
-    let item = document.createElement("li");
+    const item = document.createElement("li");
     item.setAttribute("class", "to-do-item");
 
     // checkbox
-    let checkbox = document.createElement("input");
+    const checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
     item.appendChild(checkbox);
 
     // label
-    let label = document.createElement("label");
+    const label = document.createElement("label");
     label.textContent = value;
     item.appendChild(label);
 
     // delete button
-    let del = document.createElement("button");
+    const del = document.createElement("button");
     del.setAttribute("class", "delete-button");
     del.innerHTML = "<img src=\"images/remove.png\">";
     item.appendChild(del);
 
-    del.onclick = function() {
+    del.addEventListener("click", () => {
         list.removeChild(item);
-    }
-
+    });
 
     // append item to the list
     list.appendChild(item);
 }
+
+append.addEventListener("click", addTask);
